8fc529b96c7f7387d50716448c9eea3dcab26d9c
bogeholm <troelsmikkelsen@gmail.com>
2020-11-16 21:20:48 +0100

    system.stateVersion added by installer

--- nix/darwin-configuration.nix
+++ nix/darwin-configuration.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
@@ -15,4 +15,7 @@
   # https://github.com/LnL7/nix-darwin/blob/56f01699fbe462ae9f361ff08d2dbb9e898b9439/tests/programs-zsh.nix
   programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
 }
