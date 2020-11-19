ce171f00de604d48108fcd7a1ca862f3560a4987
bogeholm <troelsmikkelsen@gmail.com>
2020-11-20 00:12:27 +0100

    Add ci.sh and make code live up to it

---
+++ ci.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set -x
+
+# Line lenght comfortable on 13" MacBook Pro in VS Code
+black . --line-length 105
+
+flake8
+
+mypy --config-file=mypy.ini sync/
+
+pylint sync/
+
+python -m pytest
--- sync/sync.py
+++ sync/sync.py
@@ -1,10 +1,15 @@
-import git
-import toml
+"""Create symbolic links from present repo to files on host"""
 
 from collections import OrderedDict
 from datetime import datetime
 from pathlib import Path
 from typing import Dict
+
+import git
+import toml
+
+BACKUP_SUFFIX = "dotfile.sync.backup"
+DATETIME_FORMAT = "%d-%b-%Y-%H-%M-%S"
 
 
 def git_repo_path() -> str:
@@ -15,23 +20,22 @@ def git_repo_path() -> str:
 
 
 def timestamp() -> str:
-    DATETIME_FORMAT = "%d-%b-%Y-%H-%M-%S"
+    """Return a timestamp in the module specified format"""
     return datetime.now().strftime(DATETIME_FORMAT)
 
 
 def backup_target_file_if_exists(target: Path) -> None:
     """If target exists, and is not a symlink, back it up as:
-        `f"{filename}.{timestamp()}.{BACKUP_SUFFIX}`
+    `f"{filename}.{timestamp()}.{BACKUP_SUFFIX}`
     """
     # TODO: Return enum representing backup status
-    BACKUP_SUFFIX = "dotfile.sync.backup"
 
     if not target.exists():
         return
-    
+
     if target.is_symlink():
         return
-    
+
     if target.is_file():
         new_target_name = f"{target.name}.{timestamp()}.{BACKUP_SUFFIX}"
         new_target = target.parent / new_target_name
@@ -41,7 +45,7 @@ def backup_target_file_if_exists(target: Path) -> None:
 
 def expand_path(path_template: str) -> Path:
     """Expand placeholders in path template string"""
-    expanders: Dict[str, Path] = {"${REPO}": git_repo_path(), "${USER_HOME}": str(Path.home())}
+    expanders: Dict[str, str] = {"${REPO}": git_repo_path(), "${USER_HOME}": str(Path.home())}
 
     for variable, value in expanders.items():
         path_template = path_template.replace(variable, value)
@@ -49,37 +53,27 @@ def expand_path(path_template: str) -> Path:
 
 
 class PathMap:
+    """Sync (link) files from repository to user's home."""
+
     def __init__(self, source: str, destination: str):
-        self.source: Path = source
-        self.destination: Path = destination
-
-    @property
-    def source(self) -> Path:
-        return self._source
-
-    @source.setter
-    def source(self, source: str) -> None:
-        self._source = expand_path(source)
-
-    @property
-    def destination(self) -> Path:
-        return self._destination
-
-    @destination.setter
-    def destination(self, destination: str) -> None:
-        self._destination = expand_path(destination)
+        self.source: Path = expand_path(source)
+        self.destination: Path = expand_path(destination)
 
     def link(self) -> None:
         """Create symlink from self.source to self.destination."""
         # TODO: Return enum representing sync status
         if not self.source.exists():
-            return False
+            return
 
         # Ensure destination parent directory exists
         self.destination.parent.mkdir(parents=True, exist_ok=True)
         # Backup existing files if needed
         backup_target_file_if_exists(self.destination)
-        # Now we can link: https://docs.python.org/3/library/pathlib.html?highlight=link#pathlib.Path.symlink_to
+        # Remove destination if it is a symlink
+        if self.destination.is_symlink():
+            self.destination.unlink()
+        # Now we can link:
+        # https://docs.python.org/3/library/pathlib.html?highlight=link#pathlib.Path.symlink_to
         self.destination.symlink_to(self.source)
 
     def __repr__(self):
@@ -90,16 +84,16 @@ class PathMap:
 
 
 if __name__ == "__main__":
-    file_list = toml.load("testfiles.toml")
+    file_list = toml.load(Path(__file__).parent / "testfiles.toml")
 
     res = OrderedDict()
     for title, paths in file_list.items():
         res[title] = PathMap(**paths)
-    
+
     print(timestamp())
     for title, pathmap in res.items():
-        #print(title)
-        #print(pathmap)
-        #print(pathmap.destination.parent)
-        #print()
+        print(title)
+        print(pathmap)
+        # print(pathmap.destination.parent)
+        # print()
         pathmap.link()
