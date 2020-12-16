19f15f08048032b65344cdb4d4507d3f4c89f816
bogeholm <troelsmikkelsen@gmail.com>
2020-12-16 21:57:06 +0100

    Update LaTeX update script

--- update/update-tex.sh
+++ update/update-tex.sh
@@ -1,6 +1,14 @@
 #! /bin/zsh
 set -x
 
+append_path_if_exists() {
+    if [ -d "${1}" ]; then
+        export PATH="${1}:${PATH}"
+    fi
+}
+
+append_path_if_exists "/usr/local/texlive/2020/bin/x86_64-darwin"
+
 # LaTeX - requires sudo
 # https://tex.stackexchange.com/questions/55437/how-do-i-update-my-tex-distribution
 tlmgr update --self
