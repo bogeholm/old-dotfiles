6f6a7f639e35070aee017139c7d10097bbde1607
bogeholm <troelsmikkelsen@gmail.com>
2021-06-01 22:49:05 +0200

    Configure build users in darwin-configuration.nix

--- nix/darwin-configuration.nix
+++ nix/darwin-configuration.nix
@@ -23,11 +23,19 @@
   services.nix-daemon.enable = false;
   nix.package = pkgs.nix;
 
-  # Enable zsh 
+  # Enable zsh
   # https://github.com/LnL7/nix-darwin/blob/56f01699fbe462ae9f361ff08d2dbb9e898b9439/tests/programs-zsh.nix
   programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  # Next line is to avoid:
+  # warning: Detected old style nixbld users
+  # These can cause migration problems when upgrading to certain macOS versions
+  # Running the installer again will remove and recreate the users in a way that avoids these problems
+  # $ darwin-install
+  # or enable to automatically manage the users
+  users.nix.configureBuildUsers = true;
 }
